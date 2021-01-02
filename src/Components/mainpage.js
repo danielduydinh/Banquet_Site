d498d8918cb63ae9ff42487b24c8d8404c4f6fd5
Kevin Xu <kelxu@ucsc.edu>
2021-01-02 15:19:26 -0800

    changing tabs now change url and display on the main page

--- src/Components/mainpage.js
+++ src/Components/mainpage.js
@@ -10,6 +10,18 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/core/styles';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+/**
+ * Resources consulted:
+ * react route tutorial https://reactrouter.com/web/example/basic
+ * Tab and Link https://stackoverflow.com/questions/51313969/using-material-ui-tabs-as-navbar
+ */
 
 const theme = createMuiTheme({
   palette: {
@@ -46,44 +58,82 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function MainPage() {
+
+  const [value, setValue] = React.useState('home');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   const classes = useStyles();
   console.log(theme);
   return (
     <React.Fragment>
       <CssBaseline />
-      <ThemeProvider theme={theme}>
-      <AppBar position="relative" className={classes.appBar}>
-        <Tabs aria-label="simple tabs example">
-          <Tab label="Item One"/>
-          <Tab label="Item Two"/>
-          <Tab label="Item Three"/>
-        </Tabs>
-      </AppBar>
-      <main color="#424242">
-        {/* Hero unit */}
-        <div className={classes.heroContent}>
-          <Container maxWidth="sm">
-            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-              Main Page
-            </Typography>
-            <div className={classes.heroButtons}>
-              <Grid container spacing={2} justify="center">
-                <Grid item>
-                  <Button variant="contained" color="primary">
-                    Button 1
-                  </Button>
+      <Router>
+        <ThemeProvider theme={theme}>
+        <AppBar position="relative" className={classes.appBar}>
+          <Tabs aria-label="simple tabs example" value={value} onChange={handleChange}>
+            <Tab label="Home" to="/" component={Link}/>
+            <Tab label="Item One" to="/one" component={Link}/>
+            <Tab label="Item Two" to="/two" component={Link}/>
+          </Tabs>
+        </AppBar>
+        <main color="#424242">
+          {/* Hero unit */}
+          <div className={classes.heroContent}>
+            <Container maxWidth="sm">
+              <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+                Main Page
+              </Typography>
+              <div className={classes.heroButtons}>
+                <Grid container spacing={2} justify="center">
+                  <Grid item>
+                    <Button variant="contained" color="primary">
+                      Button 1
+                    </Button>
+                  </Grid>
+                  <Grid item>
+                    <Button variant="outlined" color="primary">
+                      Button 2
+                    </Button>
+                  </Grid>
                 </Grid>
-                <Grid item>
-                  <Button variant="outlined" color="primary">
-                    Button 2
-                  </Button>
-                </Grid>
-              </Grid>
-            </div>
-          </Container>
-        </div>
-      </main>
-      </ThemeProvider>
+              </div>
+            </Container>
+          </div>
+        </main>
+        <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/one" component={One} />
+            <Route exact path="/two" component={Two} />
+          </Switch>
+        </ThemeProvider>
+      </Router>
     </React.Fragment>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function One() {
+  return (
+    <div>
+      <h2>One</h2>
+    </div>
+  );
+}
+
+function Two() {
+  return (
+    <div>
+      <h2>Two</h2>
+    </div>
   );
 }
