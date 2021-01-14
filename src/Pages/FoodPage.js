9d41852f922e42c261150fd5038877ac39cd2e71
Daniel Vu <danielduydinh@gmail.com>
2021-01-13 16:36:50 -0800

    Got GridList to Appear on the page. Still having problems with pictures

--- src/Pages/FoodPage.js
+++ src/Pages/FoodPage.js
@@ -6,6 +6,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
 
 const theme = createMuiTheme({
   palette: {
@@ -39,23 +41,80 @@ const useStyles = makeStyles((theme) => ({
   PageButtons: {
     marginTop: theme.spacing(4),
   },
-}));
-
-/*
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    overflow: 'hidden',
-    backgroundColor: theme.palette.background.paper,
-  },
   gridList: {
     width: 500,
     height: 450,
   },
 }));
-*/
+
+const tileData = [
+  {
+    img: '../Photos/dat.jpg',
+    title: 'Breakfast',
+    author: 'jill111',
+    cols: 2,
+    featured: true,
+  },
+  {
+    img: '../Photos/DanielPFP.jpg',
+    title: 'Tasty burger',
+    author: 'director90',
+  },
+  {
+    img: '../Photos/foood.jpg',
+    title: 'Camera',
+    author: 'Danson67',
+  },
+  {
+    img: '../Photos/dat.jpg',
+    title: 'Morning',
+    author: 'fancycrave1',
+    featured: true,
+  },
+  {
+    img: '../Photos/DanielPFP.jpg',
+    title: 'Hats',
+    author: 'Hans',
+  },
+  {
+    img: '../Photos/foood.jpg',
+    title: 'Honey',
+    author: 'fancycravel',
+  },
+  {
+    img: '../Photos/dat.jpg',
+    title: 'Vegetables',
+    author: 'jill111',
+    cols: 2,
+  },
+  {
+    img: '../Photos/DanielPFP.jpg',
+    title: 'Water plant',
+    author: 'BkrmadtyaKarki',
+  },
+  {
+    img: '../Photos/foood.jpg',
+    title: 'Mushrooms',
+    author: 'PublicDomainPictures',
+  },
+  {
+    img: '../Photos/dat.jpg',
+    title: 'Olive oil',
+    author: 'congerdesign',
+  },
+  {
+    img: '../Photos/DanielPFP.jpg',
+    title: 'Sea star',
+    cols: 2,
+    author: '821292',
+  },
+  {
+    img: '../Photos/foood.jpg',
+    title: 'Bike',
+    author: 'danfador',
+  },
+];
+
 
 export default function Foodpage() {
   const classes = useStyles();
@@ -66,6 +125,15 @@ export default function Foodpage() {
           <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
             Food Page
           </Typography>
+          <div className={classes.root}>
+            <GridList cellHeight={160} className={classes.gridList} cols={3}>
+              {tileData.map((tile) => (
+                <GridListTile key={tile.img} cols={tile.cols || 1}>
+                <img src={tile.img} alt={tile.title} />
+                </GridListTile>
+                ))}
+            </GridList>
+          </div>
           <div className={classes.PageButtons}>
             <Grid container spacing={2} justify="center">
               <Grid item>
