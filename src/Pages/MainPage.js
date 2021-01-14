363d7021ce9b3ca427360b9958f02bd758f18c5c
Ernest So <ernestkso1125@gmail.com>
2021-01-13 21:43:24 -0800

    Have pictures on home page

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -4,7 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/core/styles';
-import Carousel from 'react-elastic-carousel';
+import Carousel from 'react-bootstrap/Carousel'
 
 const theme = createMuiTheme({
   palette: {
@@ -39,28 +39,54 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const items = [
-    {id: 1, title: 'item #1'},
-    {id: 2, title: 'item #2'},
-    {id: 3, title: 'item #3'},
-    {id: 4, title: 'item #4'},
-    {id: 5, title: 'item #5'},
-];
-
 export default function Mainpage() {
   const classes = useStyles();
+  const [index, setIndex] = React.useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
   return (
-    <ThemeProvider theme={theme}>
-      <div className={classes.PageContent}>
-        <Container maxWidth="sm">
-          <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-            Main Page
-          </Typography>
-          <Carousel itemsToShow={1}>
-            {items.map(item => <div key={item.id}>{item.title}</div>)}
-          </Carousel>
-        </Container>
-      </div>
-    </ThemeProvider>
-  )
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://dogtime.com/assets/uploads/2018/10/puppies-cover.jpg"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://d17fnq9dkz9hgj.cloudfront.net/uploads/2020/04/shelter-dog-cropped-1-632x329.jpg"
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://images2.minutemediacdn.com/image/upload/c_fill,g_auto,h_1248,w_2220/v1584388937/shape/mentalfloss/536413-gettyimages-1077470274.jpg?itok=NoDcW5uz"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
 }
+
