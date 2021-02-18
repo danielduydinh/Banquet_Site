0c15ca313356d5b810e1a50374cb809c88e107ec
Ernest So <ernestkso1125@gmail.com>
2021-02-17 20:57:46 -0800

    added prototype of card

--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -15,6 +15,9 @@ import { spacing } from '@material-ui/system';
 import Paper from '@material-ui/core/Paper';
 import tDance from '../Photos/tdance.png';
 import GP from './Photos/AboutUs.jpg';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent'
 
 const theme = createMuiTheme({
   typography: {
@@ -73,9 +76,13 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(8, 0, 6),
     width: '100vw',
     minHeight: '85vh',
+    flexGrow: 1,
+    maxWidth: '100%',
+    paddingBottom: '300px'
   },
   PageButtons: {
-    marginTop: theme.spacing(4),
+    marginTop: theme.spacing(2),
+    paddingBottom: '50px',
   },
   carouseldiv: {
     width: '600px',
@@ -97,12 +104,28 @@ const useStyles = makeStyles((theme) => ({
   },
   photopadding:{
     paddingTop: "12vh",
-  }
+  },
+  card: {
+    minWidth: 275,
+    maxWidth: 400,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
 }));
 
 export default function AboutUs() {
   const classes = useStyles();
   const [index, setIndex] = React.useState(0);
+  const bull = <span className={classes.bullet}>•</span>;
 
   const handleSelect = (selectedIndex, e) => {
     setIndex(selectedIndex);
@@ -144,6 +167,27 @@ export default function AboutUs() {
             </Typography>
           </Box>
         </Container>
+        <Card className={classes.card} variant="outlined">
+          <CardContent>
+            <Typography className={classes.title} color="textSecondary" gutterBottom>
+              Word of the Day
+            </Typography>
+            <Typography variant="h5" component="h2">
+              be{bull}nev{bull}o{bull}lent
+            </Typography>
+            <Typography className={classes.pos} color="textSecondary">
+              adjective
+            </Typography>
+            <Typography variant="body2" component="p">
+              well meaning and kindly.
+              <br />
+              {'"a benevolent smile"'}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
       </div>
     </ThemeProvider>
   )
